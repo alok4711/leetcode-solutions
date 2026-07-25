11c8c3396b141abab5f12b969c67bf8df24cb8c5
alok4711 <alokkrwork@gmail.com>
2026-07-25 17:09:43 +0530

    Time: 7 ms (20.54%), Space: 46.9 MB (5.7%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -2,14 +2,12 @@ class Solution {
     public int singleNumber(int[] nums) {
         Map<Integer, Integer> map=new HashMap<>();
         for (int num : nums) {
-            map.merge(num, 1, Integer::sum);
+            map.put(num, map.getOrDefault(num, 0)+1);
         }
         int ans=0;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            int key = entry.getKey();
-            int value = entry.getValue();
-            if(value==1){
-                ans=key;
+        for (int num : nums) {
+            if(map.get(num)==1){
+                ans=num;
             }
         }
         return ans;
