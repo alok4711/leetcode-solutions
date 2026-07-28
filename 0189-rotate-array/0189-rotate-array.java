9feea018f5a9b6404c5affebfcc570ce666579ef
alok4711 <alokkrwork@gmail.com>
2026-07-29 00:30:30 +0530

    Time: 1 ms (41.25%), Space: 63.5 MB (8.1%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int len=nums.length;
+        if(k>len) k%=len;
+        int[] arr=new int[k];
+        int j=0;
+        for(int i=len-k; i<len; i++){
+            arr[j++]=nums[i];
+        }
+        for(int i=len-1; i>=k; i--){
+            nums[i]=nums[i-k];
+        }
+        for(int i=0; i<k; i++){
+            nums[i]=arr[i];
+        }
+    }
+}
