85a55af3a851dfda99c98972f4dc84639813982a
alok4711 <alokkrwork@gmail.com>
2026-07-30 22:10:47 +0530

    Time: 0 ms (100%), Space: 42.5 MB (51.37%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isHappy(int n) {
         if(n==1) return true;
-        if(n<5) return false;
+        if(n==4) return false;
         int num=0;
         while(n!=0){
             int a=n%10;
