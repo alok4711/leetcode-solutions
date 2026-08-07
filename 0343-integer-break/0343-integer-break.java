58130da9809edbb109604c86787e68a7a7a7c942
alok4711 <alokkrwork@gmail.com>
2026-08-08 00:48:11 +0530

    Time: 0 ms (100%), Space: 42.2 MB (21.36%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.java
@@ -0,0 +1,17 @@
+class Solution {
+    static int[] dp;
+    public int integerBreak(int n) {
+        dp=new int[n+1];
+        return helper(n);
+    }
+    int helper(int n){
+        if(dp[n]!=0) return dp[n];
+        if(n==2 || n==1) return dp[n]=1;
+        int ans=1;
+        for(int i=(n/2)+(n%2); i>=1; i--){
+            ans=Math.max(ans, i*(n-i));
+            ans=Math.max(ans, i*helper(n-i));
+        }
+        return dp[n]=ans;
+    }
+}
